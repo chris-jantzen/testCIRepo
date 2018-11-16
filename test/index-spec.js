e9eaf23ac7661c2025006ce6221738a41854ee30
Chris Jantzen <jantzeca@mail.uc.edu>
2018-11-16 18:46:15 -0500

    ci test

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,10 +1,16 @@
 const assert = require('assert');
 const calculator = require('../index');
-describe('calculator', function() {
-    describe('add function', function() {
-        it('adds numbers', function () {
+describe('calculator', () => {
+    describe('add function', () => {
+        it('adds numbers', () => {
             let result = calculator.add(1, 1);
             assert.equal(result, 2);
+        });
+    });
+    describe('subtract function', () => {
+        it('subtracts numbers', () => {
+            let result = calculator.subtract(2, 1);
+            assert.equal(result, 1);
         });
     });
 });
